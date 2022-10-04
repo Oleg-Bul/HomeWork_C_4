654aca2db1d89d3cc5a183f2ec3fad375ca83998
Oleg_bul <kokseg63@gmail.com>
2022-10-04 11:05:57 +0400

    Task 43 complete

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -2,3 +2,18 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
 // значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double ReadData(string msg)
+{
+    Console.Write(msg);
+    double number = double.Parse(Console.ReadLine());
+    return number;
+}
+double b1 = ReadData("Введите b1: ");
+double k1 = ReadData("Введите k1: ");
+double b2 = ReadData("Введите b2: ");
+double k2 = ReadData("Введите k2: ");
+
+double x=(b2-b1)/(k1-k2);
+double y=(k1*(b2-b1))/(k1-k2)+b1;
+System.Console.WriteLine($" Точка пересечения прямых ({x};{y})");
